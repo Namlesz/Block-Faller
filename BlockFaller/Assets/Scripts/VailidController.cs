1ff33830b10e26709e4c72c4c3106cf4617bcfe9
Adam Wiśniewski <nejmlesz@gmail.com>
2019-08-19 15:57:01 +0200

    Fast Changes

---
+++ BlockFaller/Assets/Scripts/VailidController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VailidController : MonoBehaviour
+{
+    GameObject square;
+    public float rotation = 0;
+    public float rotateSpeed;
+    private bool isRotating;
+    private void Start()
+    {
+        isRotating = false;
+        square = GameObject.Find("SquareValid");
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (rotation >= 360 || rotation <= -360)
+                rotation = 0;
+
+            isRotating = true;
+
+            if (Input.mousePosition.x > Screen.width / 2)
+            {
+                rotation -= 90;
+            }
+
+            if (Input.mousePosition.x < Screen.width / 2)
+            {
+                rotation += 90;
+            }
+        }
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        if (isRotating)
+        {
+            Vector3 to = new Vector3(0,0, rotation);
+            if (Vector3.Distance(transform.eulerAngles, to) > 0.01f)
+            {
+                transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime*rotateSpeed);
+            }
+            else
+            {
+                transform.eulerAngles = to;
+                isRotating = false;
+            }
+        }
+    }
+}
