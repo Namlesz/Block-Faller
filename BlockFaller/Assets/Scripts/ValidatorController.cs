ff39f48c3dd847ccaadb7d9d6d6aa54b44829af1
Adam Wiśniewski <nejmlesz@gmail.com>
2019-08-20 14:45:28 +0200

    Add post proccesing

--- BlockFaller/Assets/Scripts/ValidatorController.cs
+++ BlockFaller/Assets/Scripts/ValidatorController.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class ValidatorController : MonoBehaviour
 {
+    Variable vars;
+    ChromaticAberration aberration;
+    private void Start()
+    {
+        vars = GameObject.Find("GameController").GetComponent<Variable>();
+
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
+        aberration = ScriptableObject.CreateInstance<ChromaticAberration>();
+        aberration.enabled.Override(true);
+        aberration.intensity.Override(1f);
+
+        Variable.shakeCam = true;
+        Destroy(PostProcessManager.instance.QuickVolume(GameObject.Find("PPV").layer, 100f, aberration),vars.destroyEffecttime);
+
         if (CompareTag(col.tag))
         {
             Variable.points++;
--- BlockFaller/Assets/Scripts/Variable.cs
+++ BlockFaller/Assets/Scripts/Variable.cs
@@ -6,9 +6,12 @@ public class Variable : MonoBehaviour
 {
     public float speed;
     public float spawnTime;
+    public float destroyEffecttime;
     public static int points;
+    public static bool shakeCam;
     private void Start()
     {
         points = 0;
+        shakeCam = false;
     }
 }
