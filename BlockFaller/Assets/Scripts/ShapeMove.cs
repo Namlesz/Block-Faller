f94cef6ed6e018c8a16c966d09480776fb99e1a9
Adam Wiśniewski <45044261+Namlesz@users.noreply.github.com>
2019-08-27 21:36:27 +0200

    Fast fix

--- BlockFaller/Assets/Scripts/ShapeMove.cs
+++ BlockFaller/Assets/Scripts/ShapeMove.cs
@@ -7,7 +7,7 @@ public class ShapeMove : MonoBehaviour
     void Start()
     {
         Variable vars = GameObject.Find("GameController").GetComponent<Variable>();
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(0,-vars.speed)*Time.deltaTime*1000);
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0,-vars.speed)*Time.fixedDeltaTime*1000);
     }
     private void LateUpdate()
     {
